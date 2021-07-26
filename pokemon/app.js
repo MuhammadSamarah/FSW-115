bd9406695ab2a058f2ce3a70a309f5ec9cbda84e
MuhammadSamarah <muhammad.samarah@bryanuniversity.edu>
2021-07-25 19:18:24 -0500

    update

--- pokemon/app.js
+++ pokemon/app.js
@@ -1,56 +1,19 @@
+const xhr = new XMLHttpRequest()
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+    xhr.send();
 
-
-// XHR
-
-function getRandom(){
-    var xhr = new XMLHttpRequest()
-
-    xhr.open('GET', 'https://ciprand.p3p.repl.co/api?len=40&count=20', true)
-    xhr.send()
-
-    xhr.onreadystatechange = function(){
-        if(this.readyState === 4 && this.status == 200 ){
-            var data = JSON.parse(xhr.response)
-            renderRandom(data)
-        }
+xhr.onreadystatechange = function(){
+    if(xhr.readyState === 4 && xhr.status === 200){
+        const textData = xhr.responseText
+        let data = JSON.parse(textData)
+        showData(data)
     }
-}
-getRandom()
+};
 
-function renderRandom(data){
-    var container = document.getElementById('container')
-    for(let i = 0; i <= data.Strings.length; i++){
-        var newIdElement = document.createElement('li')
-        newIdElement.textContent = data.Strings[i]
-        container.appendChild(newIdElement)
+function showData(data){
+    for( i=0; i <= 10; i++){
+        const pokeName = document.createElement("h1")
+        pokeName.textContent = data.objects[0].pokemon[i].name
+        document.body.appendChild(pokeName)
     }
-}
-
-
-
-// AXIOS
-
-// Run request to get data
-const getBreweries = () => {
-    axios.get('https://api.openbrewerydb.org/breweries')
-    .then(res => {
-        renderBreweries(res.data)
-        //do more things here if you want
-    })
-    .catch(err => console.error(err) )
-}
-
-// Use this to run automatically on page mount:
-// getBreweries() 
-
-// Or use this to run on button click:
-const brewButton = document.getElementById('getBrew')
-brewButton.addEventListener('click', getBreweries)
-
-// Display data on the DOM
-function renderBreweries(breweries){
-    console.log(breweries)
-    // run for loop here to display brewery data
-
-}
-
+}; 
